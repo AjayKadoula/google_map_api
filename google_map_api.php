dc8f6276ec0566dc81badb65d54c0054b3b88b1c
Ajay Kadoula <ajay.kadoula@gmail.com>
2018-06-08 14:09:55 +0530

    google_map_api_code on php
    i have created a google map api with help of php and javascript.

---
+++ google_map_api.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Simple Map</title>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+    <style>
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+      #map {
+        height: 100%;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="map"></div>
+    <script>
+ function initMap() {
+  var london = {lat: 51.5072, lng: -0.1275};
+  var birmingham = {lat: 52.4831, lng: -1.8936};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: london,
+    scrollwheel: false,
+    zoom: 7
+  });
+
+  var directionsDisplay = new google.maps.DirectionsRenderer({
+    map: map
+  })
+  var request = {
+    destination: birmingham,
+    origin: london,
+    travelMode: google.maps.TravelMode.DRIVING
+  };
+  var directionsService = new google.maps.DirectionsService();
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+     
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
+        
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAQZGT-baEu6-dECEGMez4siW-iNYOVCM4&callback=initMap"
+    async defer></script>
+  </body>
+</html>
